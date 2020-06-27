7cc728b9113f1e9209e53da65d41352626adefe0
vd89 <me@daxcodes.in>
2020-06-27 14:31:36 +0530

    delet and single blog to view

--- controller/blogController.js
+++ controller/blogController.js
@@ -13,7 +13,48 @@ const blog_index = async (req, res) => {
 		res.json({ Msg: 'There is an error from server' });
 	}
 };
+
 const blog_create_get = (req, res) => {
 	res.render('create');
 };
-export default { blog_index, blog_create_get };
+
+const blog_create_post = async (req, res) => {
+	try {
+		const newBlog = new BlogPost(req.body);
+		await newBlog.save();
+		res.redirect('/blog');
+	} catch (error) {
+		console.log(error);
+		res.json({ ErrMsg: 'There is an error from Server' });
+	}
+};
+
+const blog_details = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const blog = await BlogPost.findById(id);
+		res.render('singleBlog', { blog });
+	} catch (error) {
+		console.log(error);
+		res.json({ ErrMsg: 'There is an error from Server' });
+	}
+};
+
+const blog_delete = async (req, res) => {
+	try {
+		const id = req.params.id;
+		await BlogPost.findByIdAndDelete(id);
+		res.redirect('/blog');
+	} catch (error) {
+		console.log(error);
+		res.json({ ErrMsg: 'There is an error from Server' });
+	}
+};
+
+export default {
+	blog_index,
+	blog_create_get,
+	blog_create_post,
+	blog_details,
+	blog_delete,
+};
--- routers/blogRoute.js
+++ routers/blogRoute.js
@@ -5,7 +5,13 @@ import BlogPost from '../models/BlogPost.js';
 import blogController from '../controller/blogController.js';
 
 const { Router } = express;
-const { blog_index, blog_create_get } = blogController;
+const {
+	blog_index,
+	blog_create_get,
+	blog_create_post,
+	blog_details,
+	blog_delete,
+} = blogController;
 
 const router = Router();
 
@@ -14,6 +20,14 @@ router.get('/', blog_index);
 
 //Create a blog
 router.get('/create', blog_create_get);
+router.post('/create', blog_create_post);
+
+// View Single Blog
+router.get('/:id', blog_details);
+
+// Delete The Blog
+router.post('/:id', blog_delete);
+
 router.post('/', async (req, res) => {
 	try {
 		const newBlog = new BlogPost(req.body);
