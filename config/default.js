ac8cabc3c164b4bc03266eaaa55b31e6156b0f32
Dixit Vora <dixitvora@gmail.com>
2021-02-14 13:23:28 +0530

    Update default.js

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@
 
 const config = {
 	mongoURI:
-		'mongodb+srv://adminDB:admin456@blogpost.8pzj6.mongodb.net/blogData?retryWrites=true&w=majority',
+		'mongodb+srv://adminDB:thisisPAssword@blogpost.8pzj6.mongodb.net/blogData?retryWrites=true&w=majority',
 	port: process.env.PORT || 8085,
 };
 export default config;
